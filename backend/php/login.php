bd6d367bfe70543c956aa74c0d7af911a7720459
Ibai <666theriver@gmail.com>
2026-03-05 20:41:39 +0100

    securizao y tal

--- backend/php/login.php
+++ backend/php/login.php
@@ -2,9 +2,17 @@
 session_start();
 require "conexion.php";
 
+/* Evita entrar directamente al login.php */
+if (!isset($_POST["email"]) || !isset($_POST["password"])) {
+    header("Location: ../../frontend/auth/login.html");
+    exit();
+}
+
+/* Recoger datos del formulario */
 $email = mysqli_real_escape_string($conn, $_POST["email"]);
 $password = mysqli_real_escape_string($conn, $_POST["password"]);
 
+/* Buscar usuario */
 $sql = "SELECT * FROM usuario WHERE email = '$email'";
 $resultado = mysqli_query($conn, $sql);
 
@@ -12,18 +20,22 @@ if (mysqli_num_rows($resultado) == 1) {
 
     $fila = mysqli_fetch_assoc($resultado);
 
+    /* Comprobar contraseña */
     if ($password == $fila["contrasenia_hash"]) {
 
         $_SESSION["id_usuario"] = $fila["id_usuario"];
         $_SESSION["nombre"] = $fila["nombre"];
         $_SESSION["rol"] = $fila["id_rol"];
 
+        /* ADMIN */
         if ($fila["id_rol"] == 2) {
 
-            header("Location: ./eleccion_admin.php");
+            header("Location: eleccion_admin.php");
             exit();
 
-        } else {
+        }
+        /* CLIENTE */
+        else {
 
             header("Location: ../../frontend/index.html");
             exit();
